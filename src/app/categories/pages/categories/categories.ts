fd044b4efe848e1dcf7d572342e6cd34173cfca6
Zubicoa Garijo <rzubicoa@minsait.com>
2026-05-06 11:45:20 +0200

    refactor: mejorar los estilos de los productos

--- src/app/categories/pages/categories/categories.ts
+++ src/app/categories/pages/categories/categories.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, signal } from '@angular/core';
+import { Component, inject, input, OnChanges, signal, SimpleChanges } from '@angular/core';
 import { ProductList } from '../../../shared/components/product-list/product-list';
 import { Product } from '../../../core/models/Product';
 import { Category } from '../../../core/models/Category';
@@ -10,14 +10,14 @@ import { ProductService } from '../../../core/services/product';
   templateUrl: './categories.html',
   styleUrl: './categories.scss',
 })
-export class Categories {
+export class Categories implements OnChanges {
   public category = input.required<Category>();
   public products = signal<Product[]>([]);
 
   private readonly productService = inject(ProductService);
-
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
     this.productService.getProductsByCategory(this.category()._id).subscribe((products) => {
+      console.log(products);
       this.products.set(products);
     });
   }
